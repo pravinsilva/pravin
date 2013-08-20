87f2f8d78780212e2423a033e4f247abb2db202d
pravinsilva <pravinsilva9000@gmail.com>
2013-08-20 14:47:24 +0530

    20/8/2013

---
+++ ticketBooking/src/Ticket.java
@@ -0,0 +1,15 @@
+
+public class Ticket {
+
+	String seatNumber;
+	String price;
+	String eventName;
+	String date;
+	String time;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
